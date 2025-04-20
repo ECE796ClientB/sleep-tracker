052c4ceb79d99e311b914a079426c5f0c182927a
Joe Balsamo <50149619+ECE796ClientB@users.noreply.github.com>
2025-04-20 06:35:35 -0400

    Update main.py

--- server/main.py
+++ server/main.py
@@ -109,6 +109,4 @@ if __name__ == '__main__':
     
     # Connect to FHIR Server and load patients and observations
     setup()
-
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
