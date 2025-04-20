f429803b05a73897d64ed37333171a2b2371b83f
Joe Balsamo <50149619+ECE796ClientB@users.noreply.github.com>
2025-04-20 12:23:33 -0400

    Update DailyInputs.tsx

--- ui/src/screens/DailyInputs.tsx
+++ ui/src/screens/DailyInputs.tsx
@@ -1,22 +1,43 @@
 import { Box, Typography, Slider, Button, Stack } from "@mui/material";
+import React, { useState } from 'react';
 import InputField from "../components/Fields/InputField";
 
 function DailyInputs() {
+
+  const [cups, setCups] = useState('');
+  const [hours, setHours] = useState('');
+
+  const handleClick = () => {
+    console.log("Button clicked!");
+    
+
+  };
+
   return (
     <Box sx={{ maxWidth: 600, mx: "auto", padding: 3 }}>
       <Typography variant="h3" gutterBottom>
         Daily Values
       </Typography>
         <Stack spacing={3}>
-          <InputField
+          <TextField
             id="caffeine"
             label="Caffeine Intake (cups of coffee)"
             type="number"
+            variant="outlined"
+            fullWidth
+            className="textField"
+            value={cups}
+            onChange={(e) => setCups(e.target.value)}
           />
-          <InputField
+          <TextField
             id="exercise"
             label="Exercise (hours spent today)"
             type="number"
+            variant="outlined"
+            fullWidth
+            className="textField"
+            value={hours}
+            onChange={(e) => setHours(e.target.value)}
           />
 
           <Typography variant="h6">Stress Level:</Typography>
@@ -34,6 +55,7 @@ function DailyInputs() {
           color="primary"
           type="submit"
           sx={{ marginTop: 3 }}
+          onClick={handleClick}
         >
           Submit
         </Button>
