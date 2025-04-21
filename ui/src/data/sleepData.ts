8079103ba608764e43e9e704aaa2fa13d1327dfc
Joe Balsamo <50149619+ECE796ClientB@users.noreply.github.com>
2025-04-20 20:19:46 -0400

    Update sleepData.ts

--- ui/src/data/sleepData.ts
+++ ui/src/data/sleepData.ts
@@ -1,7 +1,11 @@
 interface SleepDataPoint {
-  date: string;
+  day: string;
   hours: number;
   heartRate: number;
+  caffeine: number;
+  exercise: number;
+  stress: "No stress" | "Low stress" | "Moderate stress" | "High stress" | "Extremely Stressed";
+  age: number;
 }
 
 const sleepData: SleepDataPoint[] = [];
